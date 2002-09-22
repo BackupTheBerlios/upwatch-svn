6b01d0e5205f017b712d1dc6d0f005d3fb2630be
raarts <raarts@73f66349-c5de-0310-a4b6-8e58e6f7812f>
2002-09-22 16:09:56 +0000

    Moved uw_template dir to templates/probe

---
+++ upwatch/trunk/templates/probe/run.c
@@ -0,0 +1,21 @@
+#include "config.h"
+
+#include <generic.h>
+#include "cmd_options.h"
+
+int init(void)
+{
+  daemonize = TRUE;
+  every = EVERY_5SECS;
+  g_thread_init(NULL);
+  return(1);
+}
+
+int run(void)
+{
+  int ret = 0;
+
+  return(ret);
+}
+
+
