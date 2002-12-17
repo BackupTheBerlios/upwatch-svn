8181d8a486f7585c2011bf38cd310c845664dc51
raarts <raarts@73f66349-c5de-0310-a4b6-8e58e6f7812f>
2002-12-17 23:13:22 +0000

    Integrate libopts to remove the dependancy on autogen, so we don't have to install guile, umb-scheme etc for every upwatch installation

--- upwatch/trunk/autogen.sh
+++ upwatch/trunk/autogen.sh
@@ -2,11 +2,18 @@
 set -x
 
 rm -f config.cache 
+mkdir -p conf
+
+rm -rf libopts libopts-*
+gunzip -c `autoopts-config libsrc` | tar -xf -
+mv -f libopts-*.*.* libopts
+cp -fp libopts/libopts.m4 conf/.
+
 #gettextize --force --copy
 libtoolize --force --copy
-aclocal 
+aclocal  -I conf
 autoheader
 autoconf
 automake --copy --add-missing
-./configure --enable-debug $*
+./configure --with-autoopts-config=false --enable-debug $*
 
